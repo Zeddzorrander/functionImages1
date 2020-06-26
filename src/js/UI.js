a7e8b25db3d48f74c882279d5eeb917d47c0fb69
Kevin James <zeddzorrander@gmail.com>
2020-06-26 12:48:32 -0400

    generalized ggb init function and separated ui functionality

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,3 +1,5 @@
+
+
 export const elements = {
     secInput1: document.getElementById('user_inputPart1'),
     secInput2: document.getElementById('user_inputPart2'),
@@ -14,11 +16,36 @@ export const elements = {
     submit1: document.getElementById('submit1'),
     submit2: document.getElementById('submit2'),
     submit3: document.getElementById('submit3'),
+    center: document.getElementById('center'),
+    radius:document.getElementById('radius'),
+    part1: document.getElementById('part1'),
+    part2: document.getElementById('part2'),
+    part3: document.getElementById('part3')
 }
 
 
+export function updateInputSec(part) {
+    if (part === 'part1') {
+        elements.secInput1.style.display = 'block';
+        elements.secInput2.style.display = 'none';
+        elements.secInput3.style.display = 'none';
+        elements.input1.focus()
+    } else if (part === 'part2') {
+        elements.secInput1.style.display = 'none';
+        elements.secInput2.style.display = 'block';
+        elements.secInput3.style.display = 'none';
+        elements.input2.focus()
+    } else {
+        elements.secInput1.style.display = 'none';
+        elements.secInput2.style.display = 'none';
+        elements.secInput3.style.display = 'block';
+        elements.center.focus()
+    }
+}
 
-
+export function getInputValue(id) {
+    return document.getElementById(id).value
+}
 export class DOMDisplay {
     constructor(parent, level) {
         this.dom = elt('section', {class: "input", id: "user_input"}, elt('h3', {id: "h3_input"}), elt())
--- src/js/index.js
+++ src/js/index.js
@@ -2,48 +2,46 @@ import { parameters, views, pt2Codebase, pt3Codebase } from './configGeo';
 import './intervals'
 import * as ggb from './ggb'
 import '../styles.css'
-import { elements } from './UI';
+import { elements, getInputValue, updateInputSec } from './UI';
 import Interval from './intervals';
 
 let ggbApp = new GGBApplet(parameters, '5.0', views);
 
-function initPart1() {
+function initGGB(event) {
+    const part = event.target.id
+    let codebase;
     ggb.newConstruction();
-    ggb.loadCodebase(parameters.ggbBase64);
-    elements.secInput1.style.display = 'block';
-    elements.secInput2.style.display = 'none';
-    elements.secInput3.style.display = 'none';
-}
-
-function initPart2() {
-    ggb.newConstruction();
-    ggb.loadCodebase(pt2Codebase);
-    elements.secInput1.style.display = 'none';
-    elements.secInput2.style.display = 'block';
-    elements.secInput3.style.display = 'none';
-}
-
-function initPart3() {
-    ggb.newConstruction();
-    ggb.loadCodebase(pt3Codebase);
-    elements.secInput1.style.display = 'none';
-    elements.secInput2.style.display = 'none';
-    elements.secInput3.style.display = 'block';
+    if (part === 'part1') {
+        codebase = parameters.ggbBase64;
+    } else if (part === 'part2') {
+        codebase = pt2Codebase;
+    } else {
+        codebase = pt3Codebase;
+    }
+    ggb.loadCodebase(codebase);
+    updateInputSec(part);
 }
 
 function update1() {
-    const string = elements.input1.value;
+    const string = getInputValue('input1');
+    console.log(string);
     const domain = new Interval(string, 'x');
     const range = ggb.calcRange(domain);
     ggb.updateDomain(domain);
     ggb.updateRange(range);
-    
 }
 
 window.onload = function() {
     ggbApp.inject('ggbApplet', 'preferHTML5');
-    document.getElementById('part1').addEventListener('click', initPart1);
-    document.getElementById('part2').addEventListener('click', initPart2);
-    document.getElementById('part3').addEventListener('click', initPart3);
-    document.getElementById('submit1').addEventListener('click', update1)
+    elements.input1.focus();
+    elements.part1.addEventListener('click', () => {
+        initGGB(event);
+    });
+    elements.part2.addEventListener('click', () => {
+        initGGB(event);
+    });
+    elements.part3.addEventListener('click', () => {
+        initGGB(event);
+    });
+    elements.submit1.addEventListener('click', update1)
 }
