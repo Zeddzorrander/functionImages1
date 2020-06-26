9b8cfd47aaea349b46ccfa91c9768e85c117f33b
Kevin James <zeddzorrander@gmail.com>
2020-06-25 20:19:09 -0400

    added toggling of inputs based on which part selected

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,25 @@ let ggbApp = new GGBApplet(parameters, '5.0', views);
 function initPart1() {
     ggb.newConstruction();
     ggb.loadCodebase(parameters.ggbBase64);
+    document.getElementById('user_inputPart1').style.display = 'block';
+    document.getElementById('user_inputPart2').style.display = 'none';
+    document.getElementById('user_inputPart3').style.display = 'none';
 }
 
 function initPart2() {
     ggb.newConstruction();
     ggb.loadCodebase(pt2Codebase);
+    document.getElementById('user_inputPart1').style.display = 'none';
+    document.getElementById('user_inputPart2').style.display = 'block';
+    document.getElementById('user_inputPart3').style.display = 'none';
 }
 
 function initPart3() {
     ggb.newConstruction();
     ggb.loadCodebase(pt3Codebase);
+    document.getElementById('user_inputPart1').style.display = 'none';
+    document.getElementById('user_inputPart2').style.display = 'none';
+    document.getElementById('user_inputPart3').style.display = 'block';
 }
 
 window.onload = function() {
